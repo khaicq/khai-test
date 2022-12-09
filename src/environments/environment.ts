cc6e2c5073f1978dde42bebaf2b5debe40d741eb
khaicq <khaicq.0604@gmail.com>
2022-12-09 22:54:10 +0700

    update env db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   prefix: "api",
   port: 3333,
   sqlDB: {
-    name: "khai_test",
-    username: "root",
-    password: "",
+    name: "sql6583733",
+    username: "sql6583733",
+    password: "iLiXVMUiqS",
     dialect: "mysql",
-    port: 3308,
-    host: "localhost",
+    port: 3306,
+    host: "sql6.freesqldatabase.com",
   },
   token_secret:
     "6bbad18a1d97bc296c22bdb3d61f0bc8dab01e7b5e46a90fd8890a47d089b09f96e9d78c0443977fff0ea00ca6290869d93ef82503df7d16ce5362de5a616c8d",
